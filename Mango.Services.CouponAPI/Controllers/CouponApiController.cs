5e69bed4ab9d858d3448db3f9f3b4af4f19bd599
Abhinav Kumar <dynamic2106@gmail.com>
2024-10-07 00:17:12 +0530

    Added controller and get coupon endpoint

---
+++ Mango.Services.CouponAPI/Controllers/CouponApiController.cs
@@ -0,0 +1,50 @@
+﻿using Mango.Services.CouponAPI.Data;
+using Mango.Services.CouponAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mango.Services.CouponAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CouponApiController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+        public CouponApiController(AppDbContext appDbContext)
+        {
+            _context = appDbContext;
+        }
+
+        [HttpGet]
+        public object Get() 
+        
+        {
+            try
+            {
+                IEnumerable<Coupon> objList = _context.Coupons.ToList();
+                return objList;
+            }
+            catch (Exception ex) {
+                }
+            return null;
+        }
+
+
+
+        [HttpGet]
+        [Route("{id:int}")]
+        public object Get(int id)
+
+        {
+            try
+            {
+                Coupon objList = _context.Coupons.First(u=>u.CouponId == id);
+                return objList;
+            }
+            catch (Exception ex)
+            {
+            }
+            return null;
+        }
+    }
+}
