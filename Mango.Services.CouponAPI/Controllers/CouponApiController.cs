4b6adb9af0fbb6f52df80a2c47c3358fcfa9d320
Abhinav Kumar <dynamic2106@gmail.com>
2024-10-15 10:27:43 +0530

    #Added common response dto for all our endpoint

--- Mango.Services.CouponAPI/Controllers/CouponApiController.cs
+++ Mango.Services.CouponAPI/Controllers/CouponApiController.cs
@@ -1,5 +1,6 @@
 ﻿using Mango.Services.CouponAPI.Data;
 using Mango.Services.CouponAPI.Models;
+using Mango.Services.CouponAPI.Models.Dto;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -10,41 +11,49 @@ namespace Mango.Services.CouponAPI.Controllers
     public class CouponApiController : ControllerBase
     {
         private readonly AppDbContext _context;
+        private readonly ResponseDto _responseDto;
         public CouponApiController(AppDbContext appDbContext)
         {
             _context = appDbContext;
+            _responseDto = new ResponseDto();
+
         }
 
         [HttpGet]
-        public object Get() 
+        public ResponseDto Get() 
         
         {
             try
             {
-                IEnumerable<Coupon> objList = _context.Coupons.ToList();
-                return objList;
+                IEnumerable<Coupon> obj = _context.Coupons.ToList();
+                _responseDto.Result = obj;
             }
-            catch (Exception ex) {
-                }
-            return null;
+            catch (Exception ex)
+            {
+                _responseDto.IsSuccess = false;
+                _responseDto.Message = ex.Message;
+            }
+            return _responseDto;
         }
 
 
 
         [HttpGet]
         [Route("{id:int}")]
-        public object Get(int id)
+        public ResponseDto Get(int id)
 
         {
             try
             {
-                Coupon objList = _context.Coupons.First(u=>u.CouponId == id);
-                return objList;
+                Coupon obj = _context.Coupons.First(u=>u.CouponId == id);
+                _responseDto.Result = obj;
             }
             catch (Exception ex)
             {
+                _responseDto.IsSuccess = false;
+                _responseDto.Message = ex.Message;
             }
-            return null;
+            return _responseDto;
         }
     }
 }
