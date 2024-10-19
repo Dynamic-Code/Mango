164bc590aa54d84a04068bf4edd5d084d7e9dd19
Abhinav Kumar <dynamic2106@gmail.com>
2024-10-20 02:41:02 +0530

    Added Request and Response DTo for UI project

---
+++ Mango.Web/Models/ResponseDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Mango.Web.Models
+{
+    //It is a general dto class for all the response from our endpoint
+    //Common response for all our API
+    public class ResponseDto
+    {
+        public object? Result { get; set; }
+        public bool IsSuccess { get; set; } = true;
+        public string Message { get; set; } = "";
+    }
+}
