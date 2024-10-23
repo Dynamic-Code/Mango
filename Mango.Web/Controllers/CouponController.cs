2939190e40600e439367af8f8b06cfddbbd87df3
Abhinav Kumar <dynamic2106@gmail.com>
2024-10-24 00:06:24 +0530

    #Added CouponController

---
+++ Mango.Web/Controllers/CouponController.cs
@@ -0,0 +1,28 @@
+﻿using Mango.Web.Models;
+using Mango.Web.Service.IService;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace Mango.Web.Controllers
+{
+    public class CouponController : Controller
+    {
+        private readonly ICouponService _couponService;
+        public CouponController(ICouponService couponService)
+        {
+            _couponService = couponService;
+        }
+
+        public async Task<IActionResult> CouponIndex()
+        {
+            List<CouponDto>? list = new();
+            ResponseDto? res = await _couponService.GetAllCouponAsync();
+
+            if(res != null && res.IsSuccess)
+            {
+                list = JsonConvert.DeserializeObject<List<CouponDto>>(Convert.ToString(res.Result));
+            }
+            return View(list);
+        }
+    }
+}
