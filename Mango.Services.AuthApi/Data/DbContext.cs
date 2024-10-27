23e9c8426e4da0b5360a1a778c6ba67062e2faef
Abhinav Kumar <dynamic2106@gmail.com>
2024-10-27 23:32:22 +0530

    Added Auth configuration

---
+++ Mango.Services.AuthApi/Data/DbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Mango.Services.AuthApi.Data
+{
+    //In order to using Identity for auth, we will have ti implement form IdentityDbContet<IdentityUser>
+    public class AppDbContext : IdentityDbContext<IdentityUser>
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        // Seeding data in our db tables
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
