4bd5de027025b8774be15e12c7710fff273da8cd
Abhinav Kumar <dynamic2106@gmail.com>
2024-10-28 00:16:01 +0530

    Added All the Dependency in AuthServices

---
+++ Mango.Services.AuthApi/Service/AuthService.cs
@@ -0,0 +1,38 @@
+﻿using Mango.Services.AuthApi.Data;
+using Mango.Services.AuthApi.Models;
+using Mango.Services.AuthApi.Models.Dto;
+using Mango.Services.AuthApi.Service.IService;
+using Microsoft.AspNetCore.Identity;
+
+namespace Mango.Services.AuthApi.Service
+{
+    public class AuthService : IAuthService
+    {
+
+        private readonly AppDbContext _db;
+        //to do all login and register
+
+        private readonly UserManager<ApplicationUser> _userManager;
+        //To work with roles
+
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public AuthService(AppDbContext db,
+            UserManager<ApplicationUser> userManager,
+            RoleManager<IdentityRole> roleManager)
+        {
+            _db = db;
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+        public Task<LoginResponseDTO> Login(LoginRequestDTO loginRequestDTO)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<UserDTO> Register(RegisterationRequestDTO registerationRequestDTO)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Mango.Services.AuthApi/Service/IService/IAuthService.cs
@@ -0,0 +1,10 @@
+﻿using Mango.Services.AuthApi.Models.Dto;
+
+namespace Mango.Services.AuthApi.Service.IService
+{
+    public interface IAuthService
+    {
+        Task<UserDTO> Register(RegisterationRequestDTO registerationRequestDTO);
+        Task<LoginResponseDTO> Login(LoginRequestDTO loginRequestDTO);
+    }
+}
