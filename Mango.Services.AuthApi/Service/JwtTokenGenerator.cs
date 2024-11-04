81021a308df5231a0e1b3f7b97c3557f77b2623d
Abhinav Kumar <dynamic2106@gmail.com>
2024-11-04 23:48:09 +0530

    #Created a new service to generate JWT token

---
+++ Mango.Services.AuthApi/Service/JwtTokenGenerator.cs
@@ -0,0 +1,52 @@
+﻿using Mango.Services.AuthApi.Models;
+using Mango.Services.AuthApi.Models.Dto;
+using Mango.Services.AuthApi.Service.IService;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace Mango.Services.AuthApi.Service
+{
+    public class JwtTokenGenerator : IJwtTokenGenerator
+    {
+        private readonly JwtOptions _jwtOptions;
+
+        public JwtTokenGenerator(JwtOptions jwtOptions)
+        {
+            _jwtOptions = jwtOptions;
+        }
+        public string GenerateToken(ApplicationUser applicationUser)
+        {
+            // Creating new token handler
+            var tokenHandler = new JwtSecurityTokenHandler();
+            
+            // fetching key from appsetting.json
+            var key = Encoding.ASCII.GetBytes(_jwtOptions.Secret);
+
+            // creating claims
+            var claimList = new List<Claim>
+            {
+                new Claim(JwtRegisteredClaimNames.Email,applicationUser.Email),
+                new Claim(JwtRegisteredClaimNames.Sub,applicationUser.Id),
+                new Claim(JwtRegisteredClaimNames.Name,applicationUser.UserName.ToString()),
+            };
+
+            //Token descriptor to describe token or to have token config
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Audience = _jwtOptions.Audience,
+                Issuer = _jwtOptions.Issuer,
+                Subject = new ClaimsIdentity(claimList),
+                Expires = DateTime.UtcNow.AddDays(7),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256)
+
+            };
+
+            // generate token
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
+
+        }
+    }
+}
